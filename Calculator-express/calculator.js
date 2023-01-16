98d7eaff6fdae22f304136c3625cbaa9b4576b4c
Yahoo <yahya.a002@outlook.com>
2023-01-16 17:53:55 +0530

    Use of body parser and more

--- Calculator-express/calculator.js
+++ Calculator-express/calculator.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("<h1>Calculator</h1>");
+  res.sendFile(__dirname + "/calculator.html");
+});
+
+app.post("/", function (req, res) {
+  var n1 = Number(req.body.num1);
+  var n2 = Number(req.body.num2);
+
+  var result = n1 + n2;
+
+  res.send("The result is " + result);
 });
 
 app.listen(8080, function () {
